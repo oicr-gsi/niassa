03fa77c0472e614fc809297777a24e401c7276d9
Andre Masella <andre.masella@oicr.on.ca>
2018-09-18 13:32:38 -0400

    Remove admin webservice and bump version

--- seqware-admin-webservice/src/main/java/io/seqware/webservice/adapter/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Hook up custom behaviour for conversion of timestamps.
- * 
- * @since 1.0
- */
-package io.seqware.webservice.adapter;
-
--- seqware-ext-admin-testing/src/main/java/net/sourceforge/seqware/pipeline/plugins/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Create and manipulate databases for extended testing.
- * 
- * @since 1.0
- */
-package net.sourceforge.seqware.pipeline.plugins;
-
